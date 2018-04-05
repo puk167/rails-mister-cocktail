3551fa7d80fdce62a1efc7e29214265716878e3d
Martin Tannenberger <martin.tannenberger@gmail.com>
2018-04-05 16:56:38 +0800

    created dose model and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: [:index, :show, :new, :create] # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create, :destroy] do
+    end
+  end # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
