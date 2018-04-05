60a3034136b9e5b794fe337e973aa73c1dd587d9
Martin Tannenberger <martin.tannenberger@gmail.com>
2018-04-05 18:44:23 +0800

    fixed part of dose model

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,5 @@
 class Cocktail < ApplicationRecord
-  #has_many :doses, dependent: :destroy
-  #has_many :ingredients, through: :doses, dependent: :destroy
-  validates :name, uniqueness: true, presence: true
-
+  has_many :doses, dependent: :destroy
+  has_many :ingredients, through: :doses, dependent: :destroy
+  validates :name, uniqueness: true, presence: true, allow_blank: false
 end
